7777820beb756e246856ce9247d029951588c860
capricorn <an.anurag@msn.com>
2019-05-09 20:33:58 +0530

    initial

---
+++ data_types.R
@@ -0,0 +1,18 @@
+# In contrast to other programming languages like C and java in R, the variables are not declared as some data type. The variables are assigned with R-Objects and the data type of the R-object becomes the data type of the variable. There are many types of R-objects. The frequently used ones are −
+
+# Vectors
+# Lists
+# Matrices
+# Arrays
+# Factors
+# Data Frames
+
+# Logical
+
+v <- TRUE
+print(class(v))
+
+# Numeric
+
+v <- 23.5
+print(class(v))
