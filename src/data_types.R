eb4d193cb34ba3f734d16fa38f9110a7e8f4280c
capricorn <an.anurag@msn.com>
2019-05-13 20:32:11 +0530

    variables notes added

--- src/data_types.R
+++ src/data_types.R
@@ -30,3 +30,66 @@ print(class(a))
 # raw
 r <- charToRaw('Hello')
 print(class(r))
+
+
+'vectors'
+# create a vector
+apple <- c('red', 'green', 'yellow')
+print(apple)
+
+# Get the class of a vector
+print(class(apple))
+
+
+
+'list'
+# A list is an R-object which can contain many different types of elements inside it like vectors, 
+# functions and even another list inside it.
+list1 <- list(c(2, 5, 3), 21.3, sin)
+print(list1)
+print(class(list1))
+
+'Matrix'
+# A matrix is a two-dimensional rectangular data set. 
+# It can be created using a vector input to the matrix function.
+
+m = matrix(c('a', 'a', 'b', 'c', 'b', 'a'), nrow = 2, ncol = 3, byrow = TRUE)
+print(m)
+print(class(m))
+
+'Arrays'
+# While matrices are confined to two dimensions, 
+# arrays can be of any number of dimensions. The array function takes a dim attribute 
+# which creates the required number of dimension.
+
+a <- array(c('green', 'yellow'), dim = c(3, 3, 2,))
+print(a)
+print(class(a))
+
+'Factors'
+# Factors are the r-objects which are created using a vector. 
+# It stores the vector along with the distinct values of the elements in the vector as labels. 
+# The labels are always character irrespective of whether it is numeric or character or Boolean etc. 
+# in the input vector.
+
+apple_colors <- c('green', 'green', 'yellow', 'red', 'red', 'red', 'green')
+factor_apple <- factor(apple_colors)
+print(factor_apple)
+print(nlevels(factor_apple))
+print(class(factor_apple))
+
+
+'Data Frames'
+# Data frames are tabular data objects. Unlike a matrix in data frame each column can contain different modes of data.
+# The first column can be numeric while the second column can be character and third column can be logical. 
+# It is a list of vectors of equal length.
+# Data Frames are created using the data.frame() function.'
+
+BMI <- data.frame(
+  gender = c('Male', 'Male', 'Female'),
+  height = c(152, 171.5, 165),
+  weight = c(81, 93, 78),
+  Age = c(42, 38, 26)
+)
+
+print(BMI)
---
+++ src/operators.R
@@ -0,0 +1,8 @@
+# Types of Operators
+# We have the following types of operators in R programming −
+
+# Arithmetic Operators
+# Relational Operators
+# Logical Operators
+# Assignment Operators
+# Miscellaneous Operators
